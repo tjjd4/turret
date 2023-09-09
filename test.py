ffa8d4d7c878d99348ca1392bf2450317ca47467
billy billy <seanhung2000@gmail.com>
2023-09-09 12:58:22 +0800

    Update: test.py

--- test.py
+++ test.py
@@ -1,58 +1,28 @@
-# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
 import time
 import board
 import busio
 import adafruit_mlx90640
 
-PRINT_TEMPERATURES = False
-PRINT_ASCIIART = True
-
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 
 mlx = adafruit_mlx90640.MLX90640(i2c)
-print("MLX addr detected on I2C")
-print([hex(i) for i in mlx.serial_number])
+print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
+# if using higher refresh rates yields a 'too many retries' exception,
+# try decreasing this value to work with certain pi/camera combinations
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 frame = [0] * 768
 while True:
-    stamp = time.monotonic()
     try:
         mlx.getFrame(frame)
     except ValueError:
         # these happen, no biggie - retry
         continue
-    print("Read 2 frames in %0.2f s" % (time.monotonic() - stamp))
+
     for h in range(24):
         for w in range(32):
-            t = frame[h * 32 + w]
-            if PRINT_TEMPERATURES:
-                print("%0.1f, " % t, end="")
-            if PRINT_ASCIIART:
-                c = "&"
-                # pylint: disable=multiple-statements
-                if t < 20:
-                    c = " "
-                elif t < 23:
-                    c = "."
-                elif t < 25:
-                    c = "-"
-                elif t < 27:
-                    c = "*"
-                elif t < 29:
-                    c = "+"
-                elif t < 31:
-                    c = "x"
-                elif t < 33:
-                    c = "%"
-                elif t < 35:
-                    c = "#"
-                elif t < 37:
-                    c = "X"
-                # pylint: enable=multiple-statements
-                print(c, end="")
+            t = frame[h*32 + w]
+            print("%0.1f, " % t, end="")
         print()
     print()
