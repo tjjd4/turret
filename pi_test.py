03a700578a6cd999019b31b4e041bac4bc96c4ea
billy billy <seanhung2000@gmail.com>
2023-09-13 19:11:01 +0800

    fix 7

--- pi_test.py
+++ pi_test.py
@@ -12,7 +12,7 @@ import numpy as np
 import cv2
 
 def init_mlx_sensor():
-    i2c = busio.I2C(board.SCL, board.SDA, frequency=10000)
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=400000)
     mlx = adafruit_mlx90640.MLX90640(i2c)
     mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_4_HZ
     print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
