211b83855465559d96008c7064fdcf1814e7f39e
billy billy <seanhung2000@gmail.com>
2023-09-15 11:08:37 +0800

    motor test 3

--- motor_test.py
+++ motor_test.py
@@ -3,32 +3,27 @@ import time
 
 GPIO_MOTOR1 = 12
 GPIO_MOTOR2 = 13
+GPIO_MOTOR1_FEEDBACK = 18
 
 MOTOR_PWM_FREQUENCY = 50
-MOTOR_PWM_DUTY_CYCLE_180 = 120000
-MOTOR_PWM_DUTY_CYCLE_90 = 60000
-MOTOR_PWM_DUTY_CYCLE_0 = 20000
 
 print("start")
 pi = pigpio.pi()
 pi.set_mode(GPIO_MOTOR1, pigpio.OUTPUT)
-pi.set_mode(GPIO_MOTOR2, pigpio.OUTPUT)
 
-print("start moving")
+print("frequency at 500000")
 
-pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, MOTOR_PWM_DUTY_CYCLE_90)
-pi.hardware_PWM(GPIO_MOTOR2, MOTOR_PWM_FREQUENCY, MOTOR_PWM_DUTY_CYCLE_90)
-time.sleep(2)
+pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 500000)
+time.sleep(1)
+start_time = time.time()
 pi.write(GPIO_MOTOR1, 0)
-pi.write(GPIO_MOTOR2, 0)
-
-for i in range(5):
-    print("frequency : %s" % (MOTOR_PWM_DUTY_CYCLE_90 + i*10000))
-    pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, MOTOR_PWM_DUTY_CYCLE_90)
-    time.sleep(2)
-    pi.write(GPIO_MOTOR1, 0)
-    pi.write(GPIO_MOTOR2, 0)
-    time.sleep(2)
+pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 750000)
+print("totally close to move : %s seconds ---" % (time.time() - start_time))
+time.sleep(1)
+start_time = time.time()
+pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 500000)
+pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 250000)
+print("stop freq to move : %s seconds ---" % (time.time() - start_time))
 
 pi.write(GPIO_MOTOR1, 0)
 pi.write(GPIO_MOTOR2, 0)
