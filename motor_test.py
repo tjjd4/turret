7a9216eb6ca37ba82a660cca252ec4b7361f3978
billy billy <seanhung2000@gmail.com>
2023-09-16 16:22:10 +0800

    motor test3

--- motor_test.py
+++ motor_test.py
@@ -11,12 +11,12 @@ print("start")
 pi = pigpio.pi()
 pi.set_mode(GPIO_MOTOR1, pigpio.OUTPUT)
 
+
 print("frequency at 500000")
 
 pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 500000)
 time.sleep(1)
 start_time = time.time()
-pi.write(GPIO_MOTOR1, 0)
 pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 750000)
 print("totally close to move : %s seconds ---" % (time.time() - start_time))
 time.sleep(1)
@@ -24,7 +24,7 @@ start_time = time.time()
 pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 500000)
 pi.hardware_PWM(GPIO_MOTOR1, MOTOR_PWM_FREQUENCY, 250000)
 print("stop freq to move : %s seconds ---" % (time.time() - start_time))
-
+time.sleep(1)
 pi.write(GPIO_MOTOR1, 0)
 pi.write(GPIO_MOTOR2, 0)
 pi.stop()
