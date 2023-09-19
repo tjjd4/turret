85a6732d48c59b483a3ac56b8ebeceb01a4e3c77
billy billy <seanhung2000@gmail.com>
2023-09-19 18:46:03 +0800

    thermal camera refresh rate stable

--- pi_test.py
+++ pi_test.py
@@ -5,9 +5,8 @@ import busio
 import numpy as np
 import adafruit_mlx90640
 
-
 def init_mlx_sensor():
-    i2c = busio.I2C(board.SCL, board.SDA, frequency=1600000)
+    i2c = busio.I2C(board.SCL, board.SDA, frequency=1000000)
     mlx = adafruit_mlx90640.MLX90640(i2c)
     mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_16_HZ
     print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
@@ -15,39 +14,34 @@ def init_mlx_sensor():
 
 def process_frame(frame):
     thermal_matrix = np.array(frame).reshape(24, 32)
-    
-    # Reduce noise with Gaussian blur
     blurred_matrix = cv2.GaussianBlur(thermal_matrix, (5, 5), 0)
-    
-    # Extract temperatures within the desired range
     _, thresholded_matrix = cv2.threshold(blurred_matrix, TEMP_RANGE[0], TEMP_RANGE[1], cv2.THRESH_BINARY)
     thresholded_matrix = thresholded_matrix.astype(np.uint8) * 255
-    
-    # Remove small areas from the matrix and keep only the largest one if there are multiple
     contours, _ = cv2.findContours(thresholded_matrix, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if len(contours) == 0:
         return np.zeros_like(thresholded_matrix), 0
-
-    # Sort contours by area in descending order
     contours = sorted(contours, key=cv2.contourArea, reverse=True)
-    
-    # Create an empty matrix to store the largest region
     largest_region = np.zeros_like(thresholded_matrix)
-    
-    # Draw the largest contour
     cv2.drawContours(largest_region, [contours[0]], 0, 255, thickness=cv2.FILLED)
-
     return largest_region, thermal_matrix.max()
 
 def print_results(thresholded_matrix, highest_temp):
-    location = np.where(thresholded_matrix == 255)
-    print(f"highest temp: {highest_temp} at location {location}")
-    
     for row in thresholded_matrix:
         print(", ".join(["%d" % (value//255) for value in row]))
     print("_______")
 
-def thermal_detection(show_video = False):
+def find_centroid_difference(thresholded_matrix):
+    central_point = (15, 11)
+    y_positions, x_positions = np.where(thresholded_matrix == 255)
+    if len(x_positions) == 0 or len(y_positions) == 0:
+        return None, (0, 0)
+    centroid_x = int(np.mean(x_positions))
+    centroid_y = int(np.mean(y_positions))
+    difference_x = centroid_x - central_point[0]
+    difference_y = centroid_y - central_point[1]
+    return (centroid_x, centroid_y), (difference_x, difference_y)
+
+def thermal_detection_modified(show_video=False):
     mlx = init_mlx_sensor()
     frame = [0] * 768
     frame_interval = 1.0 / 16
@@ -63,21 +57,29 @@ def thermal_detection(show_video = False):
                 read_end_time = time.time()
                 image_time = time.time()
                 thresholded_matrix, highest_temp = process_frame(frame)
+                centroid, difference_to_center = find_centroid_difference(thresholded_matrix)
+                if centroid:
+                    print(f'Centroid of temperature in range: {centroid}')
+                    print(f'Difference from the most central point: {difference_to_center}')
+                else:
+                    print('No Centroid found')
                 print_results(thresholded_matrix, highest_temp)
                 elapsed_time = time.time() - start_time
-            
                 print("--- total %s seconds ---" % (time.time() - start_time))
                 print("--- read image time %s seconds ---" % (read_end_time - start_time))
                 print("--- image process %s seconds ---" % (time.time() - image_time))
-                count+=1
+                count += 1
                 if show_video:
+                    '''
                     heatmap = cv2.applyColorMap(frame, cv2.COLORMAP_HOT)
+                    
+                    
                     cv2.imshow('heatmap', heatmap)
                     key = cv2.waitKey(1) & 0xFF
-
-                # if the `q` key is pressed, break from the lop
                     if key == ord("q"):
                         break
+                    '''
+                    pass
                 if elapsed_time < frame_interval:
                     print("Sleeping for : %s" % (frame_interval - elapsed_time))
                     time.sleep(frame_interval - elapsed_time)
@@ -99,7 +101,7 @@ if __name__ == "__main__":
     count = 0
     while True:
         try:
-            thermal_detection(show_video=True)
+            thermal_detection_modified(show_video=True)
         except Exception as e:
             pass
         except KeyboardInterrupt:
