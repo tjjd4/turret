f27ef5a5cb6353b9d11a4ee1b210caa21ac47e28
Sean <seanhung2000@gmail.com>
2023-10-06 10:13:31 +0800

    fix turret direction

--- src/turret.py
+++ src/turret.py
@@ -89,11 +89,11 @@ class Turret(object):
         
         if x > 1:
             if self.m1_pulsewidth < MOTOR_PULSEWIDTH_MAX:
-                self.m1_pulsewidth = self.m1_pulsewidth + 25
+                self.m1_pulsewidth = self.m1_pulsewidth - 25
                 t_m1 = threading.Thread(target=self.__move, args=(GPIO_MOTOR1, self.m1_pulsewidth))
         elif x < -1:
             if self.m1_pulsewidth > MOTOR_PULSEWIDTH_MIN:
-                self.m1_pulsewidth = self.m1_pulsewidth - 25
+                self.m1_pulsewidth = self.m1_pulsewidth + 25
                 t_m1 = threading.Thread(target=self.__move, args=(GPIO_MOTOR1, self.m1_pulsewidth))
         
         if y > 1:
