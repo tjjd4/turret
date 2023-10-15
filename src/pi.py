4265cd88ab8055139ccf5f255b6773a537ce4ea2
billy billy <seanhung2000@gmail.com>
2023-10-15 12:40:49 +0800

    Update: pi.py

--- src/pi.py
+++ src/pi.py
@@ -5,8 +5,14 @@ class Pi(object):
     def __init__(self):
         self.pi = pigpio.pi()
 
-    def setServoPulsewidth(self, motor, pulsewidth):
-        self.pi.set_servo_pulsewidth(motor, pulsewidth)
+    def setServoPulsewidth(self, gpio, pulsewidth):
+        self.pi.set_servo_pulsewidth(gpio, pulsewidth)
 
     def stop(self):
         self.pi.stop()
+
+    def setMode(self, gpio, mode):
+        self.pi.set_mode(gpio, mode)
+
+    def write(self, gpio, level):
+        self.pi.write(gpio, level)
