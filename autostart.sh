5e8fd3b08e921c70e1aac1ce0981fd7f148939b9
Sean <seanhung2000@gmail.com>
2023-10-15 18:21:09 +0800

    Add: autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Run `pigs t` and capture its output
+output=$(pigs t)
+
+# Check if the output contains a number using a regular expression
+if [[ "$output" =~ [0-9]+ ]]; then
+    echo "pigpiod activated: $output"
+else
+    echo "pigpiod is not activated..."
+    sudo pigpiod
+fi
+
+python3 autostart.py
